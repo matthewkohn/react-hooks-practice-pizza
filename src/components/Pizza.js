2685daf6223e75f7cf366a9d65a8e51f3495759d
MattKohn <kohney@hotmail.com>
2021-12-15 17:10:08 -0700

    Complete assignment

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,13 +1,19 @@
 import React from "react";
 
-function Pizza() {
+function Pizza({ pizza, onSelectedPizza }) {
+  const { topping, size, vegetarian } = pizza;
+
+  function handleClick() {
+    onSelectedPizza(pizza);
+  }
+
   return (
     <tr>
-      <td>{"Replace Me With Pizza Topping"}</td>
-      <td>{"Replace Me With Pizza Size"}</td>
-      <td>{"Replace Me With Vegatarian"}</td>
+      <td>{topping}</td>
+      <td>{size}</td>
+      <td>{vegetarian ? "Vegetarian" : "Not Vegetarian"}</td>
       <td>
-        <button type="button" className="btn btn-primary">
+        <button type="button" className="btn btn-primary" onClick={handleClick}>
           Edit Pizza
         </button>
       </td>
